db2b8e73a1aa53bdbd538fe354bea0804f3ba30a
okankpl <okan149@gmail.com>
2024-01-28 15:12:46 +0100

    added func redner and css classes

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 let fields = [
+    'cross',
+    null,
+    'circle',
     null,
     null,
+    'circle',
+    'circle',
     null,
-    null,
-    null,
-    null,
-    null,
-    null,
-    null,
+    'cross',
 ];
+
+function init() {
+    render();
+}
+
+function render() {
+    let tableHtml = '<table>';
+    for (let i = 0; i < 3; i++) {
+        tableHtml += '<tr>';
+        for (let j = 0; j < 3; j++) {
+            const index = i * 3 + j;
+            const symbol = fields[index] === 'cross' ? 'X' : fields[index] === 'circle' ? 'O' : '';
+            tableHtml += `<td>${symbol}</td>`;
+        }
+        tableHtml += '</tr>';
+    }
+    tableHtml += '</table>';
+
+    // Füge den HTML-Code in den Container ein
+    document.getElementById('container').innerHTML = tableHtml;
+}
+
